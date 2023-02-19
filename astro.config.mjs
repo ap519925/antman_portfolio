79600470a2d408faa909e19370cccf9c8987899f
AntMan247 <antphilly2233@gmail.com>
2023-02-19 09:30:47 -0500

    content updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'http://mtanthony.com/',
-  output: "server",
-  adapter: cloudflare()
-});
+export default defineConfig({});
